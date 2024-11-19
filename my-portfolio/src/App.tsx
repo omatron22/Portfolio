472fbb45de5910f71e791d547eee0015ee1f623c
omatron22 <omaresp35@gmail.com>
2024-11-19 10:08:37 -0800

    case sensitivity issue test

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import Project from './Pages/Project';
-import VideoGame from './Pages/VideoGame';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import VideoGame from './pages/VideoGame';
 import AppBar from './components/AppBar';
 
 function App() {
